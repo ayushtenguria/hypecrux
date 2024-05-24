b7cd26f4e412a6d9bb48f079c2d21778289eb68d
ayushtenguria <hacktiger.as@gmail.com>
2024-05-24 22:49:19 +0530

    abra

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export const Header = () => {
     <div className="flex justify-center p-32 items-center">
       <div className="flex flex-col justify-center">
         <h1 className="uppercase text-5xl md:text-[14rem] font-bold ">HYPECRUX</h1>
-        <p className="uppercase text-red-600 text-2xl md:text-6xl text-center md:-mt-16">Crux Of All Hype</p>
+        <p className="uppercase text-red-600 text-2xl md:text-6xl text-center mt-5 md:mt-16">Crux Of All Hype</p>
       </div>
     </div>
   );
