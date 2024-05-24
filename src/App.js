fcef6651c1a6084f9fe77a86cbe61d518ee59f32
ayushtenguria <hacktiger.as@gmail.com>
2024-05-25 02:44:15 +0530

    Starry bg added

--- src/App.js
+++ src/App.js
@@ -5,17 +5,45 @@ import { OnlineService } from "./components/OnlineService";
 import { OurClient } from "./components/OurCiient";
 import { Tagline } from "./components/Tagline";
 import { WhoWeAre } from "./components/WhoWeAre";
+// import "./app.css"
+
+// const generateStars = (numStars) => {
+//   const stars = [];
+//   for (let i = 0; i < numStars; i++) {
+//     const x = Math.random() * window.innerWidth;
+//     const y = Math.random() * window.innerHeight;
+//     const style = {
+//       left: `${x}px`,
+//       top: `${y}px`,
+//       animationDuration: `${Math.random() * 2 + 1}s`,
+//     };
+//     stars.push(<div key={i} className="star" style={style}></div>);
+//   }
+//   return stars;
+// };
 
 function App() {
   return (
-    <div className="text-white bg-gradient-to-r from-black to-gray-800 ">
-      <Header />
-      <WhoWeAre />
-      <OfflineService />
-      <OnlineService />
-      <OurClient />      
-      <Tagline />
-      <ConstactUs />
+    // <div className="text-white bg-gradient-to-r from-black to-gray-800 ">
+    //   <Header />
+    //   <WhoWeAre />
+    //   <OfflineService />
+    //   <OnlineService />
+    //   <OurClient />
+    //   <Tagline />
+    //   <ConstactUs />
+    // </div>
+
+    <div className="min-h-screen text-white bg-black relative overflow-hidden">
+      <div className="relative min-h-screen bg-starry bg-opacity-10">
+        <Header />
+        // <WhoWeAre />
+        // <OfflineService />
+        // <OnlineService />
+        // <OurClient />
+        // <Tagline />
+        // <ConstactUs />
+      </div>
     </div>
   );
 }
